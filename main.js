0c43cad7b26d18ce0a6da311d3e389f4b2794e47
bRENDON-lEE10 <144931412+bRENDON-lEE10@users.noreply.github.com>
2023-12-12 08:56:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,49 +1,66 @@
+
 // Soccer Quiz
 
 // Button Event Listener
 document.getElementById("btn").addEventListener("click", btnClicked);
 
 function btnClicked() {
-  let answer1 = document.getElementById("output1");
-  let answer2 = document.getElementById("output2");
-  let answer3 = document.getElementById("output3");
-  let answer4 = document.getElementById("output4");
-
-  let numCorrect = 0;
-  let score = numCorrect / 4;
-  document.getElementById("output").innerHTML = score;
+  let answer1 = document.getElementById("answer1").value.toLowerCase();
+  let answer2 = document.getElementById("answer2").value.toLowerCase();
+  let answer3 = document.getElementById("answer3").value.toLowerCase();
+  let answer4 = document.getElementById("answer4").value.toLowerCase();
+  let totalcorrect = 0;
+  let totalquestions = 4;
 
   //  Answers to Questions
   if (answer1 === "11") {
     document.getElementById("output1").innerHTML = "correct";
-    numCorrect = numCorrect + 1;
+    totalcorrect++;
   } else {
     document.getElementById("output1").innerHTML = "Wrong!";
   }
 
   if (answer2 === "90") {
     document.getElementById("output2").innerHTML = "correct";
-    numCorrect = numCorrect + 1;
+    totalcorrect++;
   } else {
     document.getElementById("output2").innerHTML = "Wrong!";
   }
 
   if (answer3 === "Messi" || answer3 === "Lionel Messi") {
     document.getElementById("output3").innerHTML = "correct";
-    numCorrect = numCorrect + 1;
+    totalcorrect++;
   } else {
     document.getElementById("output3").innerHTML = "Wrong!";
   }
 
   if (answer4 === "Argentina") {
     document.getElementById("output4").innerHTML = "correct";
-    numCorrect = numCorrect + 1;
+    totalcorrect++;
   } else {
     document.getElementById("output4").innerHTML = "Wrong!";
   }
-}
 
-// Question 1 (11)
-// Question 2 (90 minutes)
-// Question 3 (Messi)
-// Question 4 (Argentina)
+  // Calculation Output
+  document.getElementById("output").innerthml = totalcorrect;
+
+  let percent1 = (totalcorrect / totalquestions) * 100;
+  document.getElementById("output").innerHTML = percent1;
+
+  if (totalcorrect === 4) {
+    document.getElementById("output5").innerHTML = "You're a soccer star!!!";
+  }
+  if (totalcorrect === 3) {
+    document.getElementById("output5").innerHTML = "Keep up the good work.";
+  }
+  if (totalcorrect === 2) {
+    document.getElementById("output5").innerHTML = "Good try";
+  }
+  if (totalcorrect === 1) {
+    document.getElementById("output5").innerHTML = "Maybe next time";
+  }
+
+  if (totalcorrect === 0) {
+    document.getElementById("output5").innerHTML = "You need some practice.";
+  }
+}
